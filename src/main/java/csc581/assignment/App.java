2dfe4d42040d9898dfe1f046b58d2a49b74c1b11
william lei <ylei5@toromail.csudh.edu>
2024-04-30 13:51:10 -0700

    initial commit

---
+++ src/main/java/csc581/assignment/App.java
@@ -0,0 +1,13 @@
+package csc581.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
